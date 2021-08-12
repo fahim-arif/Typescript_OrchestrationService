f0d2c2b5da8ff5154b4c6930a1467c26ec5661c7
Mahesh Mahadevan <mahesh@twomatches.xyz>
2021-08-12 18:21:31 +0530

    Added Docker support

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:14.17-alpine3.14 as builder
+WORKDIR /app
+COPY package*.json ./
+COPY tsconfig.json ./
+COPY prisma ./prisma
+COPY src ./src
+RUN npm ci
+RUN npm run build
+
+## this is stage two , where the app actually runs
+FROM node:14.17-alpine3.14 as runner
+WORKDIR /app
+COPY package*.json ./
+COPY prisma ./prisma
+RUN npm ci --only=production
+RUN npm add -D prisma
+RUN npx prisma generate
+COPY --from=builder /app/dist ./dist
+COPY --chown=node:node .env .
+
+USER node
+
+EXPOSE 5000
+CMD [ "node", "dist/server.js" ]
--- src/repositories/mailer/MailerRepository.ts
+++ src/repositories/mailer/MailerRepository.ts
@@ -1,6 +1,7 @@
 import {SubscriberCreate, SubscriberGet} from '@models/mailer/Mailer';
 import {SubscriberStatus} from '@prisma/client';
 import prisma from '@repositories/prisma/PrismaClient';
+import {logger} from '@middlewares/log/Logger';
 import {NotFound} from '@utils/HttpException';
 
 export default class MailerRepository {
@@ -24,18 +25,49 @@ export default class MailerRepository {
 
   }
 
-  async create(subcscriberCreate: SubscriberCreate) : Promise<SubscriberGet> {
-    const {first_name, last_name, email, contact_no} = subcscriberCreate;
-
-    const subscriber = await prisma.subscriber.create({
-      data: {
-        first_name,
-        last_name,
+  async findByEmail(email: string) : Promise<SubscriberGet> {
+    const subscriber = await prisma.subscriber.findUnique({
+      where: {
         email,
-        contact_no,
-        status: SubscriberStatus.SUBSCRIBED,
       },
     });
+    if (subscriber) {
+      return subscriber;
+    } else {
+      throw new NotFound('Resource not found');
+    }
+
+  }
+
+  async create(subcscriberCreate: SubscriberCreate) : Promise<SubscriberGet> {
+    const {first_name, last_name, email, contact_no} = subcscriberCreate;
+    let subscriber;
+    try {
+      subscriber = await this.findByEmail(email);
+      if (subscriber) {
+        logger.info(`Subscriber with email ${email} found.Will not create new subscriber`);
+        return subscriber;
+      }
+    } catch (error) {
+      if (error instanceof NotFound) {
+        logger.info(`Subscriber with email ${email} not present. Creating  new one`);
+      } else {
+        throw error;
+      }
+    }
+
+
+    if (!subscriber) {
+      subscriber = await prisma.subscriber.create({
+        data: {
+          first_name,
+          last_name,
+          email,
+          contact_no,
+          status: SubscriberStatus.SUBSCRIBED,
+        },
+      });
+    }
     return subscriber;
   }
 
